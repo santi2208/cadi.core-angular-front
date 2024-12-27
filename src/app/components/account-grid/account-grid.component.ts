836986a7951850475b8f0f1aede9254c8ae5ddc7
Santiago Diaz <santi2208@gmail.com>
2024-12-27 19:15:55 -0300

    Fix table sorting

--- src/app/components/account-grid/account-grid.component.ts
+++ src/app/components/account-grid/account-grid.component.ts
@@ -1,53 +1,65 @@
 // account-grid.component.ts
-import { Component, OnInit } from '@angular/core';
-import { AccountService, AccountDto } from '../../services/accounts/account.service';
+import { Component, OnInit } from "@angular/core";
+import {
+  AccountService,
+  AccountDto,
+} from "../../services/accounts/account.service";
 
 @Component({
-  selector: 'app-account-grid',
-  templateUrl: './account-grid.component.html',
-  styleUrls: ['./account-grid.component.css']
+  selector: "app-account-grid",
+  templateUrl: "./account-grid.component.html",
+  styleUrls: ["./account-grid.component.css"],
 })
 export class AccountGridComponent implements OnInit {
   accounts: AccountDto[] = [];
   filteredAccounts: AccountDto[] = [];
   currentPage = 1;
-  itemsPerPage = 5;
-  searchTerm = '';
-  sortColumn: keyof AccountDto = 'id';
-  sortDirection: 'asc' | 'desc' = 'asc';
+  itemsPerPage = 10;
+  searchTerm = "";
+  sortColumn: keyof AccountDto = "id";
+  sortDirection: "asc" | "desc" = "asc";
   public Math = Math;
 
-  constructor(private accountService: AccountService) { }
+  constructor(private accountService: AccountService) {}
 
   ngOnInit(): void {
-    this.accountService.getAllAccounts().subscribe(data => {
+    this.accountService.getAllAccounts().subscribe((data) => {
       this.accounts = data;
       this.applyFilterAndSort();
     });
   }
 
   applyFilterAndSort(): void {
-    // Filtrar
-    this.filteredAccounts = this.accounts.filter(account =>
-      Object.values(account).some(value =>
-        value?.toString().toLowerCase().includes(this.searchTerm.toLowerCase())
-      )
-    );
+    try {
+      // Filtrar
+      this.filteredAccounts = this.accounts.filter((account) =>
+        Object.values(account).some((value) =>
+          value
+            ?.toString()
+            .toLowerCase()
+            .includes(this.searchTerm.toLowerCase())
+        )
+      );
 
-    // Ordenar
-    this.filteredAccounts.sort((a, b) => {
-      const valueA = a[this.sortColumn];
-      const valueB = b[this.sortColumn];
-      if (valueA < valueB) return this.sortDirection === 'asc' ? -1 : 1;
-      if (valueA > valueB) return this.sortDirection === 'asc' ? 1 : -1;
-      return 0;
-    });
+      // Ordenar
+      this.filteredAccounts.sort((a, b) => {
+        const valueA = a[this.sortColumn];
+        const valueB = b[this.sortColumn];
+        if (valueA < valueB) return this.sortDirection === "asc" ? -1 : 1;
+        if (valueA > valueB) return this.sortDirection === "asc" ? 1 : -1;
+        return 0;
+      });
 
-    // Paginar
-    this.filteredAccounts = this.filteredAccounts.slice(
-      (this.currentPage - 1) * this.itemsPerPage,
-      this.currentPage * this.itemsPerPage
-    );
+      // Paginar
+      this.filteredAccounts = this.filteredAccounts.slice(
+        (this.currentPage - 1) * this.itemsPerPage,
+        this.currentPage * this.itemsPerPage
+      );
+    } catch (error) {
+      console.log("----------applyFilterAndSort-------------");
+      console.log(error);
+      console.log("-----------------------------------------");
+    }
   }
 
   onSearchTermChange(): void {
@@ -57,15 +69,18 @@ export class AccountGridComponent implements OnInit {
 
   onSort(column: keyof AccountDto): void {
     if (this.sortColumn === column) {
-      this.sortDirection = this.sortDirection === 'asc' ? 'desc' : 'asc';
+      this.sortDirection = this.sortDirection === "asc" ? "desc" : "asc";
     } else {
       this.sortColumn = column;
-      this.sortDirection = 'asc';
+      this.sortDirection = "asc";
     }
     this.applyFilterAndSort();
   }
 
   onPageChange(page: number): void {
+    console.log("-----Page-----");
+    console.log(page);
+    console.log("----------");
     this.currentPage = page;
     this.applyFilterAndSort();
   }
