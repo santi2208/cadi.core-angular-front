b53e57d950fc7f642b03f303a52716b616ba4b38
Santiago Diaz <santi2208@gmail.com>
2024-12-30 15:45:24 -0300

    .

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -10,9 +10,9 @@ declare interface RouteInfo {
 export const ROUTES: RouteInfo[] = [
     { path: '/account-grid', title: 'Cuentas',  icon:'content_paste', class: '' },
     { path: '/movement-grid', title: 'Movimientos',  icon:'bubble_chart', class: '' },
+    { path: '/import-movement', title: 'Importacion de movimientos',  icon:'upload', class: '' },  
     { path: '/account-balance-grid', title: 'Balances Generales',  icon:'leaderboard', class: '' },
     { path: '/account-period-balance-grid', title: 'Balances Por Períodos',  icon:'timeline', class: '' },  
-    { path: '/import-movement', title: 'Importacion de movimientos',  icon:'timeline', class: '' },  
     { path: '/dashboard', title: 'Dashboard',  icon: 'dashboard', class: '' },
     { path: '/table-list', title: 'Table List',  icon:'content_paste', class: '' },
     { path: '/typography', title: 'Typography',  icon:'library_books', class: '' },
